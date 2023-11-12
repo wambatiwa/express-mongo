0ee5019c4d2b1a6ca81d5241113f24dc462cf237
wamba janvier <wambatiwajanvier@gmail.com>
2023-11-12 02:38:34 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+const path = require('path')
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://wamba:Tiwa2003@cluster0.okspiyi.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/api/stuff',stuffRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname,'images')))
+
+
+module.exports = app;
